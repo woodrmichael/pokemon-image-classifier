d16d75f7abfaef7b6c111b5244f04fee4d473873
Michael" "Wood <woodm@dh-mgmt4.hpc.msoe.edu>
2024-12-09 18:39:03 -0600

    Optimized Model

--- src/optimized-model/pokemon-optimized-model.sh
+++ src/optimized-model/pokemon-optimized-model.sh
@@ -17,13 +17,13 @@
 
 # You _must_ specify the partition. Rosie's default is the 'teaching'
 # partition for interactive nodes.  Another option is the 'batch' partition.
-#SBATCH --partition=teaching
+#SBATCH --partition=dgx
 
 # The number of nodes to request
 #SBATCH --nodes=1
 
 # The number of GPUs to request
-#SBATCH --gpus=4
+#SBATCH --gpus=8
 
 # The number of CPUs to request per GPU
 #SBATCH --cpus-per-gpu=9
@@ -52,7 +52,7 @@
 container="/data/containers/msoe-horovod-tf1.sif "
 
 # Command to run inside container
-command="horovodrun -np 4 -H localhost:4 python optimized-model.py --data /home/woodm/CSC2611/pokemon-image-classifier/stratified-data --batch_size 32 --epochs 5 --main_dir /home/woodm/CSC2611/pokemon-image-classifier/src/optimized-model --augment_data false --fine_tune false"
+command="horovodrun -np 8 -H localhost:8 python optimized-model.py --data /home/woodm/CSC2611/pokemon-image-classifier/stratified-data --batch_size 16 --epochs 10 --main_dir /home/woodm/CSC2611/pokemon-image-classifier/src/optimized-model --augment_data true --fine_tune true"
 
 # Execute singularity container on node.
 singularity exec --nv -B /data:/data ${container} /usr/local/bin/nvidia_entrypoint.sh ${command}
